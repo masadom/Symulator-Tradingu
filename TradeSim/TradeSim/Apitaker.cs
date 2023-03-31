b98f681b6a0170c336dd401e5639c1512c758788
masadom <dominikmaslanka7@gmail.com>
2023-03-31 11:28:41 +0200

    poprawka do http clienta

--- TradeSim/TradeSim/Apitaker.cs
+++ TradeSim/TradeSim/Apitaker.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms;
 
 namespace TradeSim
 {
-    public class HttpClient : System.Net.Http.HttpMessageInvoker
+    public class Apitake
     {
         // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
         static readonly HttpClient client = new HttpClient();
@@ -17,7 +17,7 @@ namespace TradeSim
             // Call asynchronous network methods in a try/catch block to handle exceptions.
             try
             {
-                HttpResponseMessage response = await client.GetAxsync("http://www.contoso.com/");
+                HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
                 // Above three lines can be replaced with new helper method below
